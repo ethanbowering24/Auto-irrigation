e501f70038ac65d1d823b25178630f16e5ed9fef
Ethan Bowering <ethanbowering24@gmail.com>
2022-08-18 15:53:49 -0400

    replaced GPIO const with proper enums

--- main/include/settings.h
+++ main/include/settings.h
@@ -8,19 +8,19 @@
 #define MAX_NUM_ZONES 4 
 
 #define SOIL_SENSOR_0 ADC1_CHANNEL_0 //GPIO 36
-#define SOIL_VALVE_0 10
+#define SOIL_VALVE_0 GPIO_NUM_10
 #define WATERING_THRESHOLD_0 50
 
 #define SOIL_SENSOR_1 ADC1_CHANNEL_3 //GPIO 39
-#define SOIL_VALVE_1 10
+#define SOIL_VALVE_1 GPIO_NUM_11
 #define WATERING_THRESHOLD_1 50
 
 #define SOIL_SENSOR_2 ADC1_CHANNEL_6 //GPIO 34
-#define SOIL_VALVE_2 10
+#define SOIL_VALVE_2 GPIO_NUM_12
 #define WATERING_THRESHOLD_2 50
 
 #define SOIL_SENSOR_3 ADC1_CHANNEL_7 //GPIO 35
-#define SOIL_VALVE_3 10
+#define SOIL_VALVE_3 GPIO_NUM_13
 #define WATERING_THRESHOLD_3 50
 
 /*-----------------------------------------------------------------------------------------------------------------*/
